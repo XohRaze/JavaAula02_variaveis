d461e2864cb2bba147793df9549519668267c2bc
XohRaze <billabongmaxter@gmail.com>
2015-04-29 08:42:52 +0100

    init commit

---
+++ src/Variaveis.java
@@ -0,0 +1,31 @@
+
+public class Variaveis {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Ola Mundo");
+		
+		int i = 10;
+		float f = 2.5f;  
+		double d = 2.5d;
+		
+		double f2 = f + d; 
+		
+		boolean boo = true;
+		
+		System.out.println("A soma é " + f2);
+		System.out.println("Boolean: " + boo);
+		
+		/*
+		 * 
+		 * 
+		 * 
+		 * 
+		 */
+	}
+
+}
